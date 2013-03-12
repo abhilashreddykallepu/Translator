7af6cab1ff1f5c159ab261fc40ab52367cacee64
siva <shiva0443@gmail.com>
2013-03-12 15:23:02 +0530

    Initial Commit

---
+++ Translator/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Translator
+//
+//  Created by Comspark on 3/12/13.
+//  Copyright (c) 2013 Comspark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Translator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Translator
+//
+//  Created by Comspark on 3/12/13.
+//  Copyright (c) 2013 Comspark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
